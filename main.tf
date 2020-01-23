4fcba0390966a1fae1091e051822e4c4063dc3f2
Caio Henrique <caio@ganex.com.br>
2020-01-23 07:28:46 -0300

    Add initial files

---
+++ main.tf
@@ -0,0 +1,202 @@
+data "aws_caller_identity" "default" {}
+data "aws_region" "default" {}
+
+locals {
+  cloudwatch_log_group_name = "${var.cloudwatch_log_group_name == "" ? "/aws/aes/domains/${var.domain_name}" : var.cloudwatch_log_group_name }"
+}
+
+resource "aws_security_group" "default" {
+  count       = "${var.enabled == "true" ? 1 : 0}"
+  vpc_id      = "${var.vpc_id}"
+  name        = "${var.domain_name}"
+  description = "Allow inbound traffic from Security Groups and CIDRs."
+  tags        = "${merge(map("Name", var.domain_name), var.tags)}"
+}
+
+resource "aws_security_group_rule" "ingress_security_groups" {
+  count                    = "${var.enabled == "true" ? length(var.security_groups) : 0}"
+  description              = "Allow inbound traffic from Security Groups"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = "${element(var.security_groups, count.index)}"
+  security_group_id        = "${join("", aws_security_group.default.*.id)}"
+}
+
+resource "aws_security_group_rule" "ingress_cidr_blocks" {
+  count             = "${var.enabled == "true" && length(var.allowed_cidr_blocks) > 0 ? 1 : 0}"
+  description       = "Allow inbound traffic from CIDR blocks"
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.allowed_cidr_blocks}"]
+  security_group_id = "${join("", aws_security_group.default.*.id)}"
+}
+
+resource "aws_security_group_rule" "egress" {
+  count             = "${var.enabled == "true" ? 1 : 0}"
+  description       = "Allow all egress traffic"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${join("", aws_security_group.default.*.id)}"
+}
+
+resource "aws_cloudwatch_log_group" "cloudwatch" {
+  count             = "${var.log_publishing_index_enabled == "true" || var.log_publishing_search_enabled == "true" || var.log_publishing_application_enabled == "true" ? 1 : 0}"
+  retention_in_days = "${var.cloudwatch_log_retention_in_days}"
+  name              = "${local.cloudwatch_log_group_name}"
+}
+
+resource "aws_cloudwatch_log_resource_policy" "cloudwatch_policy" {
+  count           = "${var.log_publishing_index_enabled == "true" || var.log_publishing_search_enabled == "true" || var.log_publishing_application_enabled == "true" ? 1 : 0}"
+  policy_name     = "${var.domain_name}"
+  policy_document = "${data.aws_iam_policy_document.elasticsearch-log-publishing-policy.json}"
+}
+
+# https://github.com/terraform-providers/terraform-provider-aws/issues/5218
+resource "aws_iam_service_linked_role" "default" {
+  count            = "${var.enabled == "true" && var.create_iam_service_linked_role == "true" ? 1 : 0}"
+  aws_service_name = "es.amazonaws.com"
+  description      = "AWSServiceRoleForAmazonElasticsearchService Service-Linked Role"
+}
+
+# Role that pods can assume for access to elasticsearch and kibana
+resource "aws_iam_role" "elasticsearch_user" {
+  count              = "${var.enabled == "true" ? 1 : 0}"
+  name               = "${var.domain_name}-service-role"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+  description        = "IAM Role to assume to access the Elasticsearch ${var.domain_name} cluster"
+  tags               = "${var.tags}"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  count = "${var.enabled == "true" ? 1 : 0}"
+
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${compact(concat(var.iam_authorizing_role_arns, var.iam_role_arns))}"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+data "aws_iam_policy_document" "elasticsearch-log-publishing-policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutLogEventsBatch",
+    ]
+
+    resources = ["${aws_cloudwatch_log_group.cloudwatch.arn}"]
+
+    principals {
+      identifiers = ["es.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_elasticsearch_domain" "default" {
+  count                 = "${var.enabled == "true" ? 1 : 0}"
+  domain_name           = "${var.domain_name}"
+  elasticsearch_version = "${var.elasticsearch_version}"
+
+  advanced_options = "${var.advanced_options}"
+
+  ebs_options {
+    ebs_enabled = "${var.ebs_volume_size > 0 ? true : false}"
+    volume_size = "${var.ebs_volume_size}"
+    volume_type = "${var.ebs_volume_type}"
+    iops        = "${var.ebs_iops}"
+  }
+
+  encrypt_at_rest {
+    enabled    = "${var.encrypt_at_rest_enabled}"
+    kms_key_id = "${var.encrypt_at_rest_kms_key_id}"
+  }
+
+  cluster_config {
+    instance_count           = "${var.instance_count}"
+    instance_type            = "${var.instance_type}"
+    dedicated_master_enabled = "${var.dedicated_master_enabled}"
+    dedicated_master_count   = "${var.dedicated_master_count}"
+    dedicated_master_type    = "${var.dedicated_master_type}"
+    zone_awareness_enabled   = "${var.zone_awareness_enabled}"
+  }
+
+  node_to_node_encryption {
+    enabled = "${var.node_to_node_encryption_enabled}"
+  }
+
+  vpc_options {
+    security_group_ids = ["${aws_security_group.default.id}"]
+    subnet_ids         = ["${var.subnet_ids}"]
+  }
+
+  snapshot_options {
+    automated_snapshot_start_hour = "${var.automated_snapshot_start_hour}"
+  }
+
+  log_publishing_options {
+    enabled                  = "${var.log_publishing_index_enabled }"
+    log_type                 = "INDEX_SLOW_LOGS"
+    cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.cloudwatch.arn}"
+  }
+
+  log_publishing_options {
+    enabled                  = "${var.log_publishing_search_enabled }"
+    log_type                 = "SEARCH_SLOW_LOGS"
+    cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.cloudwatch.arn}"
+  }
+
+  log_publishing_options {
+    enabled                  = "${var.log_publishing_application_enabled }"
+    log_type                 = "ES_APPLICATION_LOGS"
+    cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.cloudwatch.arn}"
+  }
+
+  tags = "${var.tags}"
+
+  depends_on = ["aws_iam_service_linked_role.default"]
+}
+
+data "aws_iam_policy_document" "default" {
+  count = "${var.enabled == "true" ? 1 : 0}"
+
+  statement {
+    actions = ["${distinct(compact(var.iam_actions))}"]
+
+    resources = [
+      "${join("", aws_elasticsearch_domain.default.*.arn)}",
+      "${join("", aws_elasticsearch_domain.default.*.arn)}/*",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${distinct(compact(concat(var.iam_role_arns, aws_iam_role.elasticsearch_user.*.arn)))}"]
+    }
+  }
+}
+
+resource "aws_elasticsearch_domain_policy" "default" {
+  count           = "${var.enabled == "true" ? 1 : 0}"
+  domain_name     = "${var.domain_name}"
+  access_policies = "${join("", data.aws_iam_policy_document.default.*.json)}"
+}
---
+++ variables.tf
@@ -0,0 +1,177 @@
+variable "domain_name" {
+  type        = "string"
+  description = "Name of the domain"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "enabled" {
+  type        = "string"
+  default     = "true"
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "security_groups" {
+  type        = "list"
+  default     = []
+  description = "List of security group IDs to be allowed to connect to the cluster"
+}
+
+variable "allowed_cidr_blocks" {
+  type        = "list"
+  default     = []
+  description = "List of CIDR blocks to be allowed to connect to the cluster"
+}
+
+variable "vpc_id" {
+  type        = "string"
+  description = "VPC ID"
+}
+
+variable "subnet_ids" {
+  type        = "list"
+  description = "Subnet ids"
+}
+
+variable "elasticsearch_version" {
+  type        = "string"
+  default     = "7.1"
+  description = "Version of Elasticsearch to deploy"
+}
+
+variable "instance_type" {
+  type        = "string"
+  default     = "t2.small.elasticsearch"
+  description = "Elasticsearch instance type for data nodes in the cluster"
+}
+
+variable "instance_count" {
+  description = "Number of data nodes in the cluster"
+  default     = 1
+}
+
+variable "iam_role_arns" {
+  type        = "list"
+  default     = []
+  description = "List of IAM role ARNs to permit access to the Elasticsearch domain"
+}
+
+variable "iam_authorizing_role_arns" {
+  type        = "list"
+  default     = []
+  description = "List of IAM role ARNs to permit to assume the Elasticsearch user role"
+}
+
+variable "iam_actions" {
+  type        = "list"
+  default     = []
+  description = "List of actions to allow for the IAM roles, _e.g._ `es:ESHttpGet`, `es:ESHttpPut`, `es:ESHttpPost`"
+}
+
+variable "zone_awareness_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Enable zone awareness for Elasticsearch cluster"
+}
+
+variable "ebs_volume_size" {
+  description = "Optionally use EBS volumes for data storage by specifying volume size in GB"
+  default     = 10
+}
+
+variable "ebs_volume_type" {
+  type        = "string"
+  default     = "gp2"
+  description = "Storage type of EBS volumes"
+}
+
+variable "ebs_iops" {
+  default     = 0
+  description = "The baseline input/output (I/O) performance of EBS volumes attached to data nodes. Applicable only for the Provisioned IOPS EBS volume type"
+}
+
+variable "encrypt_at_rest_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Whether to enable encryption at rest"
+}
+
+variable "encrypt_at_rest_kms_key_id" {
+  type        = "string"
+  default     = ""
+  description = "The KMS key id to encrypt the Elasticsearch domain with. If not specified, then it defaults to using the AWS/Elasticsearch service KMS key"
+}
+
+variable "cloudwatch_log_group_name" {
+  type        = "string"
+  description = "Cloudwatch log group custom name, if not specified using '/aws/aes/domains/${var.domain_name}/'"
+  default     = ""
+}
+
+variable "cloudwatch_log_retention_in_days" {
+  type        = "string"
+  description = "Cloudwatch logs retention days"
+  default     = "14"
+}
+
+variable "log_publishing_index_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Specifies whether log publishing option for INDEX_SLOW_LOGS is enabled or not"
+}
+
+variable "log_publishing_search_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Specifies whether log publishing option for SEARCH_SLOW_LOGS is enabled or not"
+}
+
+variable "log_publishing_application_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Specifies whether log publishing option for ES_APPLICATION_LOGS is enabled or not"
+}
+
+variable "automated_snapshot_start_hour" {
+  description = "Hour at which automated snapshots are taken, in UTC"
+  default     = 0
+}
+
+variable "dedicated_master_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Indicates whether dedicated master nodes are enabled for the cluster"
+}
+
+variable "dedicated_master_count" {
+  description = "Number of dedicated master nodes in the cluster"
+  default     = 0
+}
+
+variable "dedicated_master_type" {
+  type        = "string"
+  default     = "t2.small.elasticsearch"
+  description = "Instance type of the dedicated master nodes in the cluster"
+}
+
+variable "advanced_options" {
+  type        = "map"
+  default     = {}
+  description = "Key-value string pairs to specify advanced configuration options"
+}
+
+variable "create_iam_service_linked_role" {
+  type        = "string"
+  default     = "false"
+  description = "Whether to create `AWSServiceRoleForAmazonElasticsearchService` service-linked role. Set it to `false` if you already have an ElasticSearch cluster created in the AWS account and AWSServiceRoleForAmazonElasticsearchService already exists. See https://github.com/terraform-providers/terraform-provider-aws/issues/5218 for more info"
+}
+
+variable "node_to_node_encryption_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Whether to enable node-to-node encryption"
+}
